c6b34a07b0a5b5eaf24dbf7ced199ea7a5e14eb5
FIONNOROURKE-STUDENT <G00429672@atu.ie>
2024-11-04 10:01:03 +0000

    customer class setup and arraylist

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,8 +1,18 @@
 package org.example;
 
-public class Customer() {
-    char customerId;
-    char name
+public class Customer {
+    public Customer() {
+        String customerId = "";
+        String name = "";
+        String email = "";
+        String phoneNumber = "";
+    }
 
+    public void setInfo(String ID, String Name, String Email, String Phone) {
+        String customerId;
+        String name = Name;
+        String email = Email;
+        String phoneNumber = Phone;
+    }
 
 }
